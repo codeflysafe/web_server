dcf8938e1ecbcde3b2c81dff6b554fd2ab0e503a
sjhuang <hsjfans_scholar@163.com>
2022-08-16 20:25:29 +0800

    add thread pool

--- thread_pool/readme.md
+++ thread_pool/readme.md
@@ -5,3 +5,10 @@
 
 一个消费者和生成这模式，这里采用一个阻塞队列作为工作任务队列，
 然后从中获取任务
+
+
+## 知识点
+### linux中pthread_join()与pthread_detach()详解
+1. linux线程执行和windows不同，pthread有两种状态joinable状态和unjoinable状态，如果线程是joinable状态，当线程函数自己返回退出时或pthread_exit时都不会释放线程所占用堆栈和线程描述符（总计8K多）。只有当你调用了pthread_join之后这些资源才会被释放。若是unjoinable状态的线程，这些资源在线程函数退出时或pthread_exit时自动会被释放。
+2. unjoinable属性可以在pthread_create时指定，或在线程创建后在线程中pthread_detach自己, 如：pthread_detach(pthread_self())，将状态改为unjoinable状态，确保资源的释放。或者将线程置为 joinable,然后适时调用pthread_join.
+3. 其实简单的说就是在线程函数头加上 pthread_detach(pthread_self())的话，线程状态改变，在函数尾部直接 pthread_exit线程就会自动退出。省去了给线程擦屁股的麻烦。
--- thread_pool/threadpool.h
+++ thread_pool/threadpool.h
@@ -6,6 +6,7 @@
 #define WEB_SERVER_THREADPOOL_H
 #include <iostream>
 #include <pthread.h>
+#include "../lock_queue/lock_queue.h"
 
 using  namespace  std;
 
@@ -16,9 +17,11 @@ public:
     threadpool(int thread_num = 8, int max_task_num = 1000);
     ~threadpool();
     // 向任务队列里面提交任务，当达到最大任务数量的时候，可能会block
-    bool append(T *task);
+    bool submit(T *task);
 
 private:
+    /*工作线程运行的函数，它不断从工作队列中取出任务并执行之*/
+    static void *worker(void *arg);
     // 开始执行
     void run();
 
@@ -31,8 +34,10 @@ private:
     int cur_active_thread_num;
     // 线程池内活跃的最大线程数量
     int max_thread_num;
-
-
+    // 是否停止允许
+    volatile bool m_stop;
+    // 任务队列
+    lock_queue<T> queue;
 
 };
 
